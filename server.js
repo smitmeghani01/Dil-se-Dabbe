3e4a451ad55b2556eee4f9e7e0f4b7b51f96f457
Smit <smitmeghani01@gmail.com>
2022-11-29 00:39:36 +0530

    Atlas connected

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express')
 const app = express()
 const ejs = require('ejs')
@@ -12,20 +12,47 @@ const MongoDbStore = require('connect-mongo')
 const passport = require('passport')
 const Emitter = require('events')
 
-// Database connection
-mongoose.connect(process.env.MONGO_CONNECTION_URL, 
+// // Database connection
+mongoose.connect("mongodb+srv://smotbot:qwerty12@cluster0.pa8f1ce.mongodb.net/pizza", 
     { 
         useNewUrlParser: true, 
         useCreateIndex:true, 
         useUnifiedTopology: true, 
-        useFindAndModify : true 
+        // useFindAndModify : true 
     });
     const connection = mongoose.connection;
     connection.once('open', () => {
     console.log('Database connected...');
-    }).catch(err => {
+    }).on('error', err => {
         console.log('Connection failed...')
 });
+
+
+// main()
+//     .then((res) => {
+//         console.log('Connection to DB successfull');
+//     })
+//     .catch((err) => {
+//         console.log('Error connecting to DB');
+//     });
+// async function main() {
+//     await mongoose.connect('mongodb+srv://jay:jay@cluster0.ft5hddt.mongodb.net/test');
+// }
+
+
+// mongoose.connect("mongodb+srv://smotbot:qwerty12@cluster0.pa8f1ce.mongodb.net/?retryWrites=true&w=majority",
+//     {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//     },
+//     (err) => {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log("Successfully Connected");
+//         }
+//     }
+// );
 
 
 // Session store
@@ -38,13 +65,20 @@ mongoose.connect(process.env.MONGO_CONNECTION_URL,
 const eventEmitter = new Emitter()
 app.set('eventEmitter', eventEmitter)
 
+const store = MongoDbStore.create({
+    mongoUrl: 'mongodb+srv://smotbot:qwerty12@cluster0.pa8f1ce.mongodb.net/pizza',
+    client: connection.getClient()
+});
+
+store.on('error', function (e) {
+    console.log('Session store error: ', e);
+});
+
 // Session config
 app.use(session({
-    secret: process.env.COOKIE_SECRET,
+    secret: 'thisismysecret',
     resave: false,
-    store: MongoDbStore.create({
-        client: connection.getClient()
-    }),
+    store,
     saveUninitialized: false,
     cookie: { maxAge: 1000 * 60 * 60 * 24 } // 24 hour
 }))
